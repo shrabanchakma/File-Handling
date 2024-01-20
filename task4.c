52240b94da726f1323ec8790b496f0b3e7953767
Shraban Chakma <shrabanchakma328@gmail.com>
2024-01-21 01:16:34 +0600

    added program to seperate vowel consonent

--- task4.c
+++ task4.c
@@ -1,11 +1,31 @@
 #include <stdio.h>
 int main()
 {
-    FILE *txtFile = fopen("textFile1.txt", "r");
-    if (txtFile == NULL)
+    // program to separate vowel consonant
+    FILE *txtFile1 = fopen("textFile1.txt", "r");
+    FILE *txtFile2 = fopen("vowels.txt", "w");
+    FILE *txtFile3 = fopen("consonants.txt", "w");
+    if (txtFile1 == NULL || txtFile2 == NULL)
     {
         printf("ERROR! File not Found");
         return 1;
+    }
+
+    int character;
+    while ((character = getc(txtFile1)) != EOF)
+    {
+        char letter = (char)character;
+        if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' ||
+            letter == 'A' || letter == 'E' || letter == 'I' || letter == 'O' || letter == 'U')
+        {
+            fputc(letter, txtFile2);
+            fputc(' ', txtFile2);
+        }
+        else
+        {
+            fputc(letter, txtFile3);
+            fputc(' ', txtFile3);
+        }
     }
 
     return 0;

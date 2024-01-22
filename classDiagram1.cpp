4c6ca8baa36f0e19885f742953ff0dbefb3fd529
Shraban Chakma <shrabanchakma328@gmail.com>
2024-01-22 21:55:50 +0600

    added first class diagram question

---
+++ classDiagram1.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Employee
+{
+private:
+    string name;
+    double payRate;
+    const int EMPLOYEE_ID;
+    static int nextID;
+
+public:
+    const static double STARTING_PAY_RATE;
+    Employee(string name) : name(name), payRate(STARTING_PAY_RATE), EMPLOYEE_ID(nextID++){};
+    Employee(string name, double payRate) : name(name), payRate(payRate), EMPLOYEE_ID(nextID++){};
+    string getName()
+    {
+        return name;
+    };
+    int getEmployeeID()
+    {
+        return EMPLOYEE_ID;
+    }
+    double getPayRate()
+    {
+        return payRate;
+    }
+    void changeName(string name)
+    {
+        this->name = name;
+    }
+    void changePayRate(double payRate)
+    {
+        this->payRate = payRate;
+    }
+    int getNextID()
+    {
+        return nextID;
+    }
+};
+
+const double Employee::STARTING_PAY_RATE = 10;
+int Employee::nextID = 444;
+
+int main()
+{
+
+    return 0;
+}
--- task3.c
+++ task3.c
@@ -15,20 +15,12 @@ int main()
     char word[20];
     int countWord = 0;
     printf("What do you wanna search? ");
-    fgets(search, sizeof(search), stdin);
-
-    int search_len = strlen(search);
-    if (search_len > 0 && search[search_len - 1] == '\n')
-    {
-        search[search_len - 1] = '\0';
-    }
+    scanf("%s", search);
 
     while (fscanf(txtFile, "%s", word) == 1)
     {
-        printf("%s %s\n", word, search);
         if (strcasecmp(word, search) == 0)
         {
-            printf("entered");
             countWord++;
         }
     }
